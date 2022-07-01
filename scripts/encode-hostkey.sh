ae89cd5cde0e3c20b1a6c8881815bf42cd6e953c
Federico Fornari <federico.fornari@cnaf.infn.it>
2022-07-01 11:47:20 +0200

    added script to encode host key

---
+++ scripts/encode-hostkey.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ $# -eq 0 ]; then
+  echo "No arguments supplied. Exit."
+  exit 1
+else
+  FILE=$1
+  if [ ! -f "$FILE" ]; then
+    echo "$FILE does not exist. Exit."
+    exit 2
+  else
+    if ! openssl rsa -in $FILE -noout -check > /dev/null 2>&1; then
+      echo "$FILE is not a private key file. Exit."
+      exit 3
+    else
+      echo "$(cat $FILE)" | base64 -w0
+      exit 0
+    fi
+  fi
+fi
